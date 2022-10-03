6234549e8e7ba4be7087465a48479b47e008dd09
krishnanpandya007 <21dce067@charusat.edu.in>
2022-10-03 17:13:48 +0530

    fixed peerDependency Bug && removed unneccesary dependancy

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,57 @@
 import React from 'react'
-
-import { ExampleComponent } from 'use-refillable-state'
-import 'use-refillable-state/dist/index.css'
+import useRefillableState from 'use-refillable-state/src'
+// import { ExampleComponent } from 'use-refillable-state'
+// import 'use-refillable-state/dist/index.css'
+// import useRefillableState from 'use-refillable-state'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  
+  const { currentState, setCurrentState, acceptDraft, discardDraft, showingDraft, saveState } = useRefillableState({first_name: '', last_name: '', email: '', password: ''})
+
+  return (
+
+    <center style={{width: '30vw', marginTop: '30vh', marginLeft: '40vw', backgroundColor: '#EFEAD8', borderRadius: '10px', fontFamily: 'monospace'}}>
+
+      {showingDraft ? <div style={{display: 'flex', alignItems: 'center', borderRadius: '10px 10px 0 0', justifyContent: 'space-between', backgroundColor: '#00FFAB', padding: '0 1rem'}}>
+        <p style={{color: 'black'}}>Accept Saved Changes?</p>
+        <div >
+
+          <button style={{color: 'green'}} onClick={acceptDraft}>&#x2713; Accept</button> {' '}
+          <button style={{color: 'red'}} onClick={discardDraft}>&#x2717; Ignore</button>
+
+        </div>
+      </div> : <p style={{textAlign: 'left', backgroundColor: '#00FFAB', borderRadius: '10px 10px 0 0', display: 'flex', alignItems: 'center', padding: '1rem 1rem'}}>Long/Complex Form</p>}
+      <br/>
+
+      {/* <h3>Important/Long/Complex Form</h3> */}
+      <form>
+
+
+        <input placeholder="First Name" disabled={showingDraft} type="text" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.first_name} onChange={(e) => { setCurrentState({...currentState, first_name: e.target.value}) }} />
+        <br/>
+        <input placeholder="Last Name" disabled={showingDraft} type="text" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.last_name} onChange={(e) => { setCurrentState({...currentState, last_name: e.target.value}) }} />
+        <br/>
+
+        <input placeholder="Email" disabled={showingDraft} type="email" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.email} onChange={(e) => { setCurrentState({...currentState, email: e.target.value}) }} />
+        <br/>
+
+        <input placeholder="Password" disabled={showingDraft} type="password" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.password} onChange={(e) => { setCurrentState({...currentState, password: e.target.value}) }} />
+        <br/>
+
+        <br/>
+        <span style={{color: '#c4c4c4', fontFamily: 'monospace'}}>//Other radio/checkbox/mixed inputs</span>
+        <br/>
+        <br/>
+
+        <button disabled={showingDraft} onClick={(e) => {e.preventDefault(); saveState();}}>Save Externally</button>
+        <br/>
+        <br/>
+
+      </form>
+
+    </center>
+
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,158 @@
 import React from 'react'
-import styles from './styles.module.css'
+// import {useHistory} from "react-router-dom"
+function unMarshalData(data, type){
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  switch (type) {
+
+    case 'string':
+      return data;
+
+    case 'number':
+      return Number(data);
+
+    case 'object':
+      return JSON.parse(data);
+
+    case 'boolean':
+      return Boolean(data)
+    
+    default: 
+      return data;
+
+  }
+
 }
+
+function useRefillableState(initial_state){
+
+  /*
+
+  Args: content_key_hardcoded => Do not use useId() from react as it can generate different values as client refreshes page/component
+
+  */
+
+  const [givenState, setGivenState] = React.useState(initial_state)
+  const [draftState, setDraftState] = React.useState(null)
+  const [currentState, setCurrentState] = React.useState(givenState)
+  const [ showState, setShowState] = React.useState('current') // current | draft
+
+
+  const unique_key = typeof window !== 'undefined' ? window.location.pathname.endsWith('/') && window.location.pathname.length > 1 ? window.location.pathname.slice(0, -1) : window.location.pathname : null;
+  const unique_key_type = typeof initial_state;
+
+  const changeGivenState = (new_state) => {
+
+    console.log("curr:", new_state)
+
+    setGivenState(new_state);
+    // if(showState === 'current'){
+      setCurrentState(new_state)
+    // } else {
+    // }
+
+  } 
+
+  const SyncStateLocally = () => {
+
+    try{
+
+      localStorage.setItem(unique_key + '__type', typeof currentState)
+      localStorage.setItem(unique_key, typeof currentState === 'object' ? JSON.stringify(currentState) : currentState);
+    
+    } catch (e) {
+
+      console.error("Error Syncing data locally: ", e)
+
+    }
+
+
+  }
+
+  const DiscardDraft = () => {
+
+    setShowState('current');
+    setCurrentState(givenState);
+    setDraftState(null)
+
+    try{
+
+      localStorage.removeItem(unique_key)
+      localStorage.removeItem(unique_key + '__type')
+
+    } catch (e) {
+
+      console.error("Can't remove saved draft::", e)
+
+    }
+
+  }
+
+  const AcceptDraft = () => {
+
+    setShowState('current');
+    setCurrentState(draftState);
+
+  }
+
+  const handleWindowBeforeUnload = (e) => {
+
+    e.preventDefault()
+    // SyncStateLocally();
+    console.log("Helllo")
+    try{
+
+      localStorage.setItem(unique_key + '__type', typeof currentState)
+      localStorage.setItem(unique_key, typeof currentState === 'object' ? JSON.stringify(currentState) : currentState);
+    
+    } catch (e) {
+
+      console.error("Error Syncing data locally: ", e)
+
+    }
+    // alert("yes")
+    // alert("Hello")
+    e.returnValue = "asdasd"
+    return 'ass';
+
+  }
+
+
+  React.useEffect(() => {
+
+    if(typeof localStorage !== 'undefined'){
+
+      const saved_draft = localStorage.getItem(unique_key);
+
+      if(saved_draft !=  null && saved_draft != undefined){
+
+        const saved_draft_type = localStorage.getItem(unique_key + '__type')
+        if(saved_draft_type !=  null && saved_draft_type != undefined){
+          console.log("DEBUG_",draftState)
+          setShowState('draft')
+          setDraftState(unMarshalData(saved_draft, saved_draft_type))
+          console.log("sad", unMarshalData(saved_draft, saved_draft_type))
+          setCurrentState(unMarshalData(saved_draft, saved_draft_type));
+
+        }
+
+
+      }
+
+    }
+
+  }, [])
+
+  return {
+
+    setCurrentState: changeGivenState,
+    acceptDraft: AcceptDraft,
+    discardDraft: DiscardDraft,
+    showingDraft: showState === 'draft',
+    currentState: currentState,
+    saveState: SyncStateLocally
+
+  }
+
+}
+
+export default useRefillableState;
