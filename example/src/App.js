f6b1d7cbeb34da213ea6eff96b4604a9c37d2645
krishnanpandya007 <21dce067@charusat.edu.in>
2022-10-16 23:47:38 +0530

    BREAKING_CHANGE: version 2.0.0 early version

--- example/src/App.js
+++ example/src/App.js
@@ -1,58 +1,132 @@
-import React from 'react'
-import useRefillableState from 'use-refillable-state/src'
-// import { ExampleComponent } from 'use-refillable-state'
-// import 'use-refillable-state/dist/index.css'
-// import useRefillableState from 'use-refillable-state'
+import React, { useEffect } from "react"
+import { useRefillableForm } from "use-refillable-state/src";
 
 const App = () => {
-  
-  const { currentState, setCurrentState, acceptDraft, discardDraft, showingDraft, saveState } = useRefillableState({first_name: '', last_name: '', email: '', password: ''})
+
+  const { formRef, showingDraft, deleteDraft, acceptDraft, discardDraft, currentState, setCurrentState } = useRefillableForm();
+  const [blobUrl, setBlobUrl] = React.useState('')
+
+
+  const handleFormSubmit = (e) => {
+
+    let submission_successfull = make_submission(currentState);
+    
+    if(submission_successfull){
+      deleteDraft();
+    }
+
+  }
+
+  useEffect(() => {
+    if(currentState['profile_resume']){
+
+      setBlobUrl(URL.createObjectURL(currentState['profile_resume']))
+    }
+  }, [currentState['profile_resume']])
+
 
   return (
     <center>
-    <center style={{width: '30vw', marginTop: '35vh', backgroundColor: '#EFEAD8', borderRadius: '10px', fontFamily: 'monospace'}}>
+      {/* can use display flex, align i... */}
 
-      {showingDraft ? <div style={{display: 'flex', alignItems: 'center', borderRadius: '10px 10px 0 0', justifyContent: 'space-between', backgroundColor: '#002E94', padding: '0 1rem'}}>
-        <p style={{color: 'black'}}>Accept Saved Changes?</p>
-        <div >
 
-          <button style={{color: 'green'}} onClick={acceptDraft}>&#x2713; Accept</button> {' '}
-          <button style={{color: 'red'}} onClick={discardDraft}>&#x2717; Ignore</button>
+      {showingDraft && <div className="draft_confirm_alert">
+        <p>Accept Drafted Changes?</p>
+        <div>
+          {/* <button> */}
 
+          <svg onClick={acceptDraft} color="#2B7A0B" width="23" height="23" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+
+          {/* </button> */}
+          {/* <button> */}
+
+          <svg onClick={discardDraft} color="#E64848" width="23" height="23" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+
+          {/* </button> */}
         </div>
-      </div> : <p style={{textAlign: 'left', backgroundColor: '#002E94', borderRadius: '10px 10px 0 0', display: 'flex', alignItems: 'center', padding: '1rem 1rem'}}>Long/Complex Form</p>}
-      <br/>
-
-      {/* <h3>Important/Long/Complex Form</h3> */}
-      <form>
+      </div>}
+      <form onSubmit={handleFormSubmit} ref={formRef}>
 
 
-        <input placeholder="First Name" disabled={showingDraft} type="text" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.first_name} onChange={(e) => { setCurrentState({...currentState, first_name: e.target.value}) }} />
-        <br/>
-        <input placeholder="Last Name" disabled={showingDraft} type="text" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.last_name} onChange={(e) => { setCurrentState({...currentState, last_name: e.target.value}) }} />
-        <br/>
+        <h2>Provide Details</h2>  
+        <input placeholder="First Name" type="text" name="first_name" />
+        <input placeholder="Last Name" type="text" name="last_name" />
+        <input placeholder="Email" type="email" name="email" />
+        <input placeholder="Phone No." type="phone" name="phone_no" />
+        <input placeholder="Password" type="password" name="phone_no" />
 
-        <input placeholder="Email" disabled={showingDraft} type="email" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.email} onChange={(e) => { setCurrentState({...currentState, email: e.target.value}) }} />
-        <br/>
+        {
+        
+        currentState['profile_resume']  
+              ? <div style={{ width: 'min(500px, 50vw)', border: '1px solid rgba(0, 0, 0, 0.5)', padding: '0 0.8rem 1rem 0.8rem', margin: '0.7rem 1rem' }}>
+                  <p >Preview File: {currentState['profile_resume']?.name}</p>
+                  <img style={{height: '100px', borderRadius: '5px'}}  src={`${blobUrl}`} />
+                </div>
+              : <input placeholder="File" type="file" name="profile_resume" />
+              
+        }
 
-        <input placeholder="Password" disabled={showingDraft} type="password" style={{color: showingDraft ? 'lightgreen' : ''}} value={currentState.password} onChange={(e) => { setCurrentState({...currentState, password: e.target.value}) }} />
-        <br/>
+        <div>
+          <b>Which Language you love the most? </b>
+          <select defaultValue="python" name="loved_language" onChange={(e) => {setCurrentState(curr => {return {...curr, [e.target.name]: e.target.value}});e.persist()}}>
+            <option value="python">Python</option>
+            <option value="js">JS</option>
+            <option value="c">C</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
 
-        <br/>
-        <span style={{color: '#c4c4c4', fontFamily: 'monospace'}}>//Other radio/checkbox/mixed inputs</span>
-        <br/>
-        <br/>
+        <div>
+          <i>Choose you fav. OTT </i>
+          <input type="radio" id="Netflix" name="brand" value="Netflix"/>
+          <label for="Netflix">Netflix</label>
+          
+          <input type="radio" id="AP" name="brand" value="AP"/>
+          <label for="AP">Amazon Prime</label>
 
-        <button disabled={showingDraft} onClick={(e) => {e.preventDefault(); saveState();}}>Save Externally</button>
-        <br/>
-        <br/>
+          <input type="radio" id="hotstar" name="brand" value="hotstar"/>
+          <label for="hotstar">Hotstar</label>
+        </div>
 
+        <div>
+        <input placeholder="Agree TnC" type="checkbox" name="agree_tnc" defaultChecked={true} />
+        <span>I Agree Terms and Conditions</span>
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+          <button type="reset">Reset</button>
+        </div>
       </form>
-
     </center>
-    </center>
-
   )
+
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function make_submission (e) {return true}
--- src/index.js
+++ src/index.js
@@ -1,87 +1,173 @@
-import React from 'react'
-// import {useHistory} from "react-router-dom"
-function unMarshalData(data, type){
+import React, { useEffect, useRef } from 'react'
 
-  switch (type) {
 
-    case 'string':
-      return data;
 
-    case 'number':
-      return Number(data);
-
-    case 'object':
-      return JSON.parse(data);
-
-    case 'boolean':
-      return Boolean(data)
-    
-    default: 
-      return data;
-
-  }
-
-}
-
-function useRefillableState(initial_state){
+export default function useRefillableState(initial_state, form_unique_key=''){
 
   /*
 
-  Args: content_key_hardcoded => Do not use useId() from react as it can generate different values as client refreshes page/component
+
+  Args: {key: value,...}
+  
+    * `value` must be any of [String, Number, Boolean, Blob, Json]
+    * form_unique_key => If you use this hook more than once per page, provide harcoded value (preffer string over numbers/other).
+!                     => Do not use useId() hook from react to provide form_unique_key as it can generate different values as client reloads page
+!                     => form_unique_key must be harcoded / unchangable
+  
+
+
+  Quick Handshake Guide:
+
+
+    ** About Unique Identifier for each state/form:
+    
+        * Global Format: " {page_url_encoded} / {form_unique_key_encoded} " => value (current_state)
+
+        * xyz_encoded => encodeURIComponent(xyz)
+
 
   */
 
-  const [givenState, setGivenState] = React.useState(initial_state)
-  const [draftState, setDraftState] = React.useState(null)
-  const [currentState, setCurrentState] = React.useState(givenState)
-  const [ showState, setShowState] = React.useState('current') // current | draft
+  const [ currentState, setCurrentState ] = React.useState(initial_state);
+  const [ draftState, setDraftState ] = React.useState(null);
+  const [ mode, setMode ] = React.useState('current'); // current | draft
+
+  const db = React.useRef(null);
 
 
-  const unique_key = typeof window !== 'undefined' ? window.location.pathname.endsWith('/') && window.location.pathname.length > 1 ? window.location.pathname.slice(0, -1) : window.location.pathname : null;
-  const unique_key_type = typeof initial_state;
+  const unique_key = typeof window !== 'undefined' 
+      ?window.location.pathname.endsWith('/') && window.location.pathname.length > 1
+          ? encodeURIComponent(window.location.pathname.slice(0, -1)) + form_unique_key
+              ? "/"+encodeURIComponent(String(form_unique_key)) 
+              : "" 
+          : encodeURIComponent(window.location.pathname) + form_unique_key 
+            ? "/"+encodeURIComponent(String(form_unique_key)) 
+            : "" 
+      : null;
 
-  const changeGivenState = (new_state) => {
+  
+  
+  const configureIndexedDB = () => {
 
-    console.log("curr:", new_state)
+    const indexedDB = window.indexedDB;
 
-    setGivenState(new_state);
-    // if(showState === 'current'){
-      setCurrentState(new_state)
-    // } else {
-    // }
+    const request = indexedDB.open("urs_form_context_db", 1);
 
-  } 
+    request.onerror = function (event) {
 
-  const SyncStateLocally = () => {
+        console.error("Error creating/opening Database")
+        console.error(event)
+    }
 
-    try{
+    request.onupgradeneeded = () => {
 
-      localStorage.setItem(unique_key + '__type', typeof currentState)
-      localStorage.setItem(unique_key, typeof currentState === 'object' ? JSON.stringify(currentState) : currentState);
+      const _db = request.result;
+            
+      const store = _db.createObjectStore("forms", { keyPath: "formIdentifier__urs" });
+      
+      
+    }
     
-    } catch (e) {
+    request.onsuccess = () => {
 
-      console.error("Error Syncing data locally: ", e)
+      const _db = request.result;
+      
+      db.current = _db
+
+      SeekDraft();
 
     }
 
-
   }
 
-  const DiscardDraft = () => {
+  const SeekDraft = () => {
 
-    setShowState('current');
-    setCurrentState(givenState);
-    setDraftState(null)
+    /* 
+      Checks if there is any before saved draft to this form or not
+        * Yes
+          - Show Drafted State to user
+        *No
+          - Continue
+    */
+
+    if(typeof db.current === null) {
+
+      console.error("Looks like DB not initialized properly")
+      return;
+
+    }
+
+    const transaction = db.current.transaction("forms", "readwrite");
+    const store = transaction.objectStore("forms");
 
     try{
 
-      localStorage.removeItem(unique_key)
-      localStorage.removeItem(unique_key + '__type')
+      const current_form_draft_query = store.get(unique_key);
+  
+      current_form_draft_query.onsuccess = () => {
+  
+        const { result } = current_form_draft_query
+  
+        try{
+          
+          delete result.formIdentifier__urs
+
+          setDraftState(result)
+          setCurrentState(result)
+          setMode('draft')
+  
+        } catch {}
+  
+      }
+    } catch {}
+
+  }
+
+  const SyncStateLocally = () => {
+
+    if(typeof db.current == null) {
+
+      console.error("Looks like DB not initialized properly")
+      return;
+
+    }
+
+    const transaction = db.current.transaction("forms", "readwrite");
+    const store = transaction.objectStore("forms");
+
+    try{
+
+      setCurrentState(curr => {store.put({...curr, formIdentifier__urs: unique_key});return curr})
+
+      
 
     } catch (e) {
 
-      console.error("Can't remove saved draft::", e)
+      console.error("Error Saving Current State: ", e);
+
+    }
+
+  }
+
+  const deleteState = () => {
+
+    if(typeof db.current == null) {
+
+      console.error("Looks like DB not initialized properly")
+      return;
+
+    }
+
+    const transaction = db.current.transaction("forms", "readwrite");
+    const store = transaction.objectStore("forms");
+
+    try{
+
+      store.delete(unique_key);
+
+    } catch (e) {
+
+      console.error("Error deleting State:", e)
 
     }
 
@@ -89,70 +175,195 @@ function useRefillableState(initial_state){
 
   const AcceptDraft = () => {
 
-    setShowState('current');
-    setCurrentState(draftState);
+    if (typeof draftState == null) return;
+    setCurrentState({...draftState})
+    setMode('current')
 
   }
 
-  const handleWindowBeforeUnload = (e) => {
+  const DiscardDraft = () => {
 
-    e.preventDefault()
-    // SyncStateLocally();
-    console.log("Helllo")
-    try{
-
-      localStorage.setItem(unique_key + '__type', typeof currentState)
-      localStorage.setItem(unique_key, typeof currentState === 'object' ? JSON.stringify(currentState) : currentState);
-    
-    } catch (e) {
-
-      console.error("Error Syncing data locally: ", e)
-
-    }
-    // alert("yes")
-    // alert("Hello")
-    e.returnValue = "asdasd"
-    return 'ass';
+    setMode('current')
 
   }
-
 
   React.useEffect(() => {
 
-    if(typeof localStorage !== 'undefined'){
 
-      const saved_draft = localStorage.getItem(unique_key);
+    if( typeof initial_state !== 'object' ) {
 
-      if(saved_draft !=  null && saved_draft != undefined){
+      throw new Error("initial_state argument must be 'object' type")
 
-        const saved_draft_type = localStorage.getItem(unique_key + '__type')
-        if(saved_draft_type !=  null && saved_draft_type != undefined){
-          console.log("DEBUG_",draftState)
-          setShowState('draft')
-          setDraftState(unMarshalData(saved_draft, saved_draft_type))
-          console.log("sad", unMarshalData(saved_draft, saved_draft_type))
-          setCurrentState(unMarshalData(saved_draft, saved_draft_type));
+    }
 
-        }
+    if(typeof window !== 'undefined'){
 
-
-      }
+      // On-Client
+      configureIndexedDB();
 
     }
 
   }, [])
 
+
   return {
 
-    setCurrentState: changeGivenState,
+    setCurrentState: setCurrentState,
+    draftState: draftState,
     acceptDraft: AcceptDraft,
     discardDraft: DiscardDraft,
-    showingDraft: showState === 'draft',
+    showingDraft: mode === 'draft',
     currentState: currentState,
-    saveState: SyncStateLocally
+    saveState: SyncStateLocally,
+    deleteState: deleteState
+
+  }
+
+
+
+}
+
+
+export function useRefillableForm() {
+
+  /*
+  
+  @Args: None
+  
+  Quick Handshake Guide:
+
+    - Default Save-Call is on blur of spammable fields like text/email/password etc...
+    - deleteDraft should be only called on (2xx responses)/depends usecases, as it cannot retrieve deleted draft
+  ! - Only select tag need to update the state, as it is not farly detectable from hook itself.
+    - Form stores uploaded digital assets as `File()` type formation, so as retrieved value.
+
+  ?NOTE: If you want more customization over state manipulation/other action calls, you can use generic hook (useRefillableState) to modify according to your usecase.
+
+  */
+
+  const { currentState, setCurrentState, draftState,acceptDraft, discardDraft, showingDraft, saveState, deleteState } = useRefillableState({})
+  const inputRefs = React.useRef([]);
+  const formRef = React.useRef();
+
+  const handleInputChange = (e, save_on_change=false) => {
+    setCurrentState(curr => {return {...curr, [e.target.name]: ['file', 'image'].includes(e.target.type) ? e.target.files[0] : e.target.type !== 'checkbox' ? e.target.value : e.target.checked }})
+    if(['file', 'image'].includes(e.type)) {saveState()}
+
+    if(save_on_change) saveState();
+
+  }
+
+  const RadioInputChange = ( e ) => {
+    setCurrentState(curr => {return {...curr, radio_inputs: { [e.target.name]: e.target.value }}})
+
+  }
+
+  const configureTag = (elem) => {
+
+    //Tracking inner input elements in DFS formation.
+    for (const child of elem.children){
+      let _catch = configureTag(child)
+    }
+
+
+    if (!['input', 'select'].includes(elem.tagName.toLowerCase()) || (["button", "submit"].includes(elem.type)) ) return false;
+
+    let listener_type = "keyup";
+
+    if ( ["checkbox", "color", "date", "datetime-local", "radio", "file", "image", "month", "radio", "range"].includes(elem.type) ){
+      listener_type = "change";
+
+    }else{
+
+      // Spammable fields save on Blur(off-Focus)
+      // Ex.. 'text', 'email', 'password'....
+      elem.addEventListener("blur", saveState);
+
+    }
+
+    if(elem.type === 'radio'){
+
+      elem.addEventListener(listener_type, RadioInputChange);
+
+    }else {
+
+      elem.addEventListener(listener_type, (e) => {handleInputChange(e, listener_type === 'change')})
+  
+      // Each time input field un-focuses/ save it!!
+    }
+    
+    
+
+
+    // If its load first time and have saved state / sync the value
+
+    inputRefs.current.push(elem);
+
+    return elem;
+
+  }
+
+  React.useEffect(() => {
+
+    Object.values(formRef.current?.children).map(configureTag);
+
+    return () => {
+
+      for(const inp in inputRefs.current){
+
+        inp?.removeEventListener("blur", saveState);
+        inp?.removeEventListener("keyup", handleInputChange);
+        inp?.removeEventListener("change", handleInputChange);
+        
+
+      }
+
+    }
+  }, [])
+
+  React.useEffect(() => {
+
+    if(showingDraft){
+      for (const inp of inputRefs.current){
+
+        if(!['file', 'image'].includes(inp.type)){
+
+          switch (inp.type){
+
+            case "checkbox":
+              inp.checked = currentState[inp.name] ?? inp.defaultChecked 
+              break;
+
+            case "radio":
+              inp.checked = inp.value === currentState.radio_inputs?.[`${inp.name}`];
+              break;
+
+            default:
+              inp.value = currentState[inp.name] ?? ""
+
+
+          }
+
+        }
+        
+        inp.disabled = true
+
+      }
+    }
+
+
+  }, [showingDraft])
+
+  return {
+
+    formRef: formRef,
+    currentState: currentState,
+    showingDraft: showingDraft,
+    setCurrentState: setCurrentState,
+    deleteDraft: deleteState,
+    acceptDraft: () => {inputRefs.current.map((inp) => {inp.disabled = false});acceptDraft();deleteState();},
+    discardDraft: () => {inputRefs.current.map((inp) => {if (inp.tagName === 'INPUT' && inp.type !== 'radio') { inp.value=""}else{inp.checked=false};inp.disabled = false});discardDraft();deleteState();setCurrentState({})}
 
   }
 
 }
-
-export default useRefillableState;
